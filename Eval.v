5cf4e437a0bd34dd77ac43ef01bd1655ee6e880e
Blaudeau Clément <clement.blaudeau@gmail.com>
2020-04-06 20:10:14 +0200

    Env/Store as lists

--- Eval.v
+++ Eval.v
@@ -4,49 +4,16 @@ Require Import List.
 Import ListNotations.
 Open Scope nat_scope.
 
-(*
-Notation "'_(' x ',' y ')'" := (Some (Some (x, y))) (at level 20).
-Notation "'Err'" := (Some None) (at level 20).
-Notation "'Timeout'" := (None).
-*)
-
-
-Definition empty_env:Env := (fun (v: Var) => None).
-Check fun l : list (Var * Tpe) => fst (split l).
-
-
-Fixpoint aux (y : Var) (x: list (Var * Tpe)) (v : list Value) {struct v} :=
-      match (x, v) with
-       | ((y', _)::xt, v::vt) => match (Nat.eqb y y') with
-                               | true  => Some v
-                               | false => aux y xt vt end
-       | _ => None end.
-Definition list_match (x: list (Var * Tpe)) (v : list Value) : Env := fun (y : Var) => aux y x v.
-Notation "x ↦ v" := (list_match x v) (at level 100).
-
-Definition empty_store := (fun (l: Loc) => None : option Obj).
-Notation "'∅'" := (empty_store) (at level 200).
-
-
-Definition update {Y: Type} (x : Loc) (v : Y) (σ: Loc -> (option Y)) :=
-  fun (y: Var) => if (Nat.eqb x y) then (Some v) else (σ y).
-Notation  "[ x ↦ v ] σ" := (update x v σ) (at level 100, x at level 90).
-
-Fixpoint update_list (y : Var) (x : list Var) (v : list Value) (σ: Var -> (option Value)) :=
-  match (x, v) with
-   | (x::xt, v::vt) => if (Nat.eqb x y) then (Some v) else update_list y xt vt σ
-   | _ => (σ y) end.
-Notation  "[ x ⟼ v ] σ" := (fun (y:Loc) => update_list y x v σ) (at level 100, x at level 90).
-
+Check fold_left.
 
 Definition assign (v0: Value) (x: Var) (v: Value) (σ: Store) : Store :=
-  match (σ v0) with
-    | Some (C, fields) => [v0 ↦ (C, [x ↦ v]fields)] σ
-    | None => ∅
+  match (getObj σ v0) with
+    | Some (C, fields) => [ v0 ↦ (C, [x ↦ v]fields)] σ
+    | None => ∅ (* ? *)
   end.
 
 Definition assign_list (v0: Value) (x: list Var) (v: list Value) (σ: Store) : Store :=
-  match (σ v0) with
+  match (getObj σ v0) with
     | Some (C, fields) => [v0 ↦ (C, [x ⟼ v]fields)] σ
     | None => ∅
   end.
@@ -60,7 +27,7 @@ Fixpoint eval (e: Expr) (ct: ClassTable) (σ: Store) (ρ: Env) (v: Value) (k: na
   | S n => match e with
           (* Var: simple lookup of the store *)
           | var x => (
-              match (ρ x) with
+              match (getVal ρ x) with
               | Some v => (Success v σ)
               | _ => Error
               end )
@@ -72,9 +39,9 @@ Fixpoint eval (e: Expr) (ct: ClassTable) (σ: Store) (ρ: Env) (v: Value) (k: na
           | fld e0 x => (
               match (⟦e0⟧(ct, σ, ρ, v)(n)) with
               | Success v0 σ1 =>
-                match (σ1 v0) with
+                match (getObj σ1 v0) with
                 | Some (c, f) =>
-                  match (f x) with
+                  match (getVal f x) with
                   | Some v1 => Success v1 σ1
                   | _ => Error end
                 | _ => Error end
@@ -84,14 +51,14 @@ Fixpoint eval (e: Expr) (ct: ClassTable) (σ: Store) (ρ: Env) (v: Value) (k: na
           | mtd e0 m e_l => (
               match (⟦e0⟧(ct, σ, ρ, v)(n)) with
               | Success v0 σ1 => (
-                  match (σ1 v0) with
+                  match (getObj σ1 v0) with
                    | Some (C, _) => (
                        match (ct C)  with
                         | Some (class _  _ methods) => (
                             match methods m with
                              | Some (method μ x _ e1) => (
                                 match (⟦_ e_l _⟧(ct, σ1, ρ, v)(n)) with
-                                 | Success_list args_val σ2 => let ρ1 := (x ↦ args_val) in
+                                 | Success_list args_val σ2 => let ρ1 := [(removeTypes x) ⟼ args_val]∅ in
                                                            ⟦e1⟧(ct, σ2, ρ1, v)(n)
                                  | r => r end)
                             | _ => Error end)
@@ -99,10 +66,15 @@ Fixpoint eval (e: Expr) (ct: ClassTable) (σ: Store) (ρ: Env) (v: Value) (k: na
                    | _ => Error end)
               | r => r end)
                              
-          (* New class
+          (* New class *)
           | new C args => (
               match (⟦_ args _⟧(ct, σ, ρ, v)(n)) with
-                | Success_list args_val σ1 => *)
+                | Success_list args_val σ1 => (
+                    let I := (length σ1) + 1 in
+                    let σ2 := [I ↦ (C, ∅)] σ1 in
+                    let σ3 := (init I args_val C ct σ2 n) in
+                    (Success I σ3))
+                | _ => Error end) (* Unknown class *) 
 
           (* Field assignement *)
           | asgn e1 x e2 e' => (
@@ -113,7 +85,7 @@ Fixpoint eval (e: Expr) (ct: ClassTable) (σ: Store) (ρ: Env) (v: Value) (k: na
                                         ⟦e'⟧(ct, σ3, ρ, v)(n))
                                     | z => z end
                 | z => z end ) 
-          | _ => (Success v σ) end
+          end
   end
 where "'⟦' e '⟧' '(' ct ',' σ ',' ρ ',' v ')(' k ')'" := (eval e ct σ ρ v k)
 with eval_list (e_l: list Expr) (ct: ClassTable) (σ: Store) (ρ: Env) (v: Value) (k: nat) :  Result :=
@@ -126,5 +98,21 @@ with eval_list (e_l: list Expr) (ct: ClassTable) (σ: Store) (ρ: Env) (v: Value
                                                      | z => z end
                             | z => z end )
                        e_l (Success_list [] σ) end
-where "'⟦_' e '_⟧' '(' ct ',' σ ',' ρ ',' v ')(' k ')'" := (eval_list e ct σ ρ v k).
+where "'⟦_' e '_⟧' '(' ct ',' σ ',' ρ ',' v ')(' k ')'" := (eval_list e ct σ ρ v k)
+with init (I : Var) (v : list Var) (C: ClN) (ct: ClassTable) (σ: Store) (k :nat) : Store :=
+   match k with
+     | 0 => ∅
+     | S n =>
+       match (ct C) with
+         | Some (class x F M) => (
+             let σ1 := (assign_list I (removeTypes x) v σ) in
+             let f  := (fun (σ: Store) (f: Field) => match f with
+                          |field x t e => (
+                             match (⟦e⟧(ct, σ, ∅, I)(n)) with
+                               | Success v1 σ1 => (assign I x v1 σ1)
+                               | _ => ∅ end) end) in
+             (fold_left f F σ)) 
+         | None => ∅
+       end
+   end.
 
--- Trees.v
+++ Trees.v
@@ -1,30 +1,29 @@
-From Coq Require Import Strings.String.
-Open Scope string_scope.
-Open Scope list_scope.
+From Coq Require Import Lists.List.
+Import ListNotations.
 
 (* Read the pen/paper proofs of scopability *)
 (* Github repo  ? *)
 
+(* Type modifiers *)
 Inductive mode: Set := hot | warm | cold.
 Notation "'@' u " := (u) (at level 20).
 Check @hot.
 
+(* Basic types *)
 Definition Var : Type := nat.
 Definition Mtd : Type := nat.
 Definition ClN : Type := nat.
 Definition Tpe : Type := ClN.
 Definition Loc : Type := nat.
 
-
+(* Expression constructors *)
 Inductive Expr: Type :=
   | var   : Var -> Expr
   | this
   | fld   : Expr -> Var -> Expr
   | mtd   : Expr -> Mtd -> (list Expr) -> Expr
   | new   : ClN -> (list Expr) -> Expr
-  | asgn  : Expr -> Var -> Expr -> Expr -> Expr
-.
-
+  | asgn  : Expr -> Var -> Expr -> Expr -> Expr.
 
 Inductive Method: Type :=
   | method(μ : mode)(args : list (Var * Tpe))(out_type : Tpe)(body :Expr).
@@ -38,19 +37,44 @@ Inductive Class: Type :=
 Inductive Program: Type :=
   | program(C: list Class)(entry: Expr).
 
-
+(* Constructs *)
 Definition Value : Type := Loc.
 Definition ClassTable: Type := (ClN -> option Class).
-Definition Obj: Type := (ClN * (Var -> option Value)).
-Definition Store: Type := (Loc -> option Obj).
-Definition Env: Type := (Var -> option Value).
+Definition Env: Type   := list Value.
+Definition Obj: Type   := (ClN * Env).
+Definition Store: Type := list Obj.
 
+(* Evaluation result *)
 Inductive Result : Type :=
   | Timeout
   | Error
   | Success : Value -> Store -> Result
   | Success_list : (list Value) -> Store -> Result.
 
+(* Helpers *)
+Definition getObj (l : list Obj):= nth_error l.
+Definition getVal (l : list Value) := nth_error l.
 
+Fixpoint removeTypes (l : list (Var*Tpe)) : (list Var) :=
+  match l with
+    | [] => []
+    | ((v, t) :: l') => (v::(removeTypes l')) end.
 
+Fixpoint update_one {X : Type} (position : nat) (value : X)(l : list X) : list X :=
+  match (l, position) with
+    | ([], _) => []
+    | (_::t, 0) => value::t
+    | (_::l', S n) => update_one n value l' end.
 
+Fixpoint update_list {X : Type} (positions : list nat) (values : list X) (l : list X) : list X :=
+  match (positions, values) with
+    | (x::xt, v::vt) => update_list xt vt (update_one x v l)
+    | (_, _) => l end.
+
+Notation "∅" := ([]).
+Notation "[ x ↦ v ] σ" := (update_one x v σ) (at level 0).
+Notation "[ x ⟼ v ] σ" := (update_list x v σ) (at level 0).
+
+Check [0].
+Check [0 ↦ 1] ∅.
+Check [[0] ⟼ [1]] [1 ; 2 ; 3].
