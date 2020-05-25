74e7ed5e55a3a6f52cc5ffe25503107ed8152b7a
Blaudeau Clément <clement.blaudeau@gmail.com>
2020-05-25 17:23:18 +0200

    Work in progress for scopability, still not happy with hypothesis choices

--- Scopability.v
+++ Scopability.v
@@ -22,126 +22,181 @@ Module Scopability.
   Import Eval.Evaluator.
   Import Reachability.Reachability.
 
-  Definition scoping (σ1 σ2: Store) (L1 L2: Ensemble Loc) := forall l, l < (dom σ1) -> (σ2 ⊫ L2 ⇝ l) -> σ1 ⊫ L1 ⇝ l.
-  Notation "a ⋖ b" := (scoping (fst b) (fst a) (snd b) (snd a)) (at level 81).
+  Definition storeSubset (σ: Store) L := (forall l, (l ∈ L) -> l < (dom σ)).
+  Notation "L ⪽ σ" := (storeSubset σ L) (at level 80).
 
-  Definition scoping_preservation (σ1 σ2: Store) L :=
+  Definition scoping (σ σ': Store) (L L': Ensemble Loc) :=
+    L ⪽ σ ->
+    L' ⪽ σ' ->
+    (forall l, l < (dom σ) -> (σ' ⊫ L' ⇝ l) -> σ ⊫ L ⇝ l).
+  Notation "a ⋖ b" := (scoping (fst a) (fst b) (snd a) (snd b)) (at level 81).
+
+  Definition scoping_preservation (σ1 σ2: Store) (L: LocSet) :=
+    (L ⪽ σ1) /\
     forall σ0 (L0 L1: Ensemble Loc),
       (dom σ0) <= (dom σ1) ->
-      (* (dom σ0) <= (dom σ2 -> *)
-      (* (forall l, (l ∈ L0) -> l < (dom σ0)) -> *)
-      (* (forall l, (l ∈ L1) -> l < (dom σ1)) -> *)
-      (σ1, L)  ⋖ (σ0, L0) ->
-      (σ1, L1) ⋖ (σ0, L0) ->
-      (σ2, L1) ⋖ (σ0, L0).
+      (dom σ0) <= (dom σ2) ->
+      L0 ⪽ σ0 ->
+      L1 ⪽ σ1 ->
+      (σ0, L0) ⋖ (σ1, L) ->
+      (σ0, L0) ⋖ (σ1, L1) ->
+      (σ0, L0) ⋖ (σ2, L1).
 
   Notation "σ1 ⇝ σ2 ⋖ L" := (scoping_preservation σ1 σ2 L) (at level 81, σ2 at level 80, L at level 80).
 
   (* Results : *)
 
-  Lemma scoping_reflexivity : forall (σ: Store) (L1 L2: Ensemble Loc), (Included Loc L1 L2) -> ((σ, L1) ⋖ (σ, L2)).
+
+  Lemma scoping_reflexivity : forall (σ: Store) (L1 L2: Ensemble Loc), (L2 ⊆ L1) -> ((σ, L1) ⋖ (σ, L2)).
   Proof.
+    intros.
     unfold scoping, reachability_set.
     simpl.
-    intros.
+    intros A1 A2 l Hdom H1.
     move: H1 => [l' [Hl'1 Hl'2]].
     exists l'; split => //.
     unfold Included in H.
     auto.
   Qed.
 
-  Lemma scoping_subset : forall σ1 σ2 L L1 L2, (σ2, L2)  ⋖ (σ1, L1) ->
-                                          L ⊆ L2 ->
-                                          (σ2, L)  ⋖ (σ1, L1).
+  Lemma scoping_subset : forall σ1 σ2 L L1 L2, (σ1, L1)  ⋖ (σ2, L2∪L) ->
+                                          L2∪L ⪽ σ2 ->
+                                          (σ1, L1)  ⋖ (σ2, L).
   Proof.
     unfold scoping, reachability_set.
     simpl.
     intros.
-    apply (H l H1).
-    move: H2 => [l' [Hl'1 Hl'2]].
+    apply H => //.
+    move: H4 => [l' [Hl'1 Hl'2]].
     exists l'; split => //.
-    unfold Included in H0.
+    apply Union_intror.
     auto.
   Qed.
 
-  Lemma scoping_union :  forall σ1 σ2 L L1 L2, (σ2, L1)  ⋖ (σ1, L) ->
-                                          (σ2, L2)  ⋖ (σ1, L) ->
-                                          (σ2, L1∪L2)  ⋖ (σ1, L).
+  Lemma scoping_union :  forall σ1 σ2 L L1 L2, (σ1, L)  ⋖ (σ2, L1) ->
+                                          (σ1, L)  ⋖ (σ2, L2) ->
+                                          (σ1, L)  ⋖ (σ2, L1∪L2).
   Proof.
     unfold scoping, reachability_set.
     simpl.
     intros.
-    move: H2 => [l' [Hl'1 Hl'2]].
-    induction Hl'1 as [l' | l']; repeat eauto ; exists l'.
+    move: H4 => [l' [Hl'1 Hl'2]].
+    move /(_ H1):H => H.
+    move /(_ H1):H0 => H0.
+    induction Hl'1 as [l' | l'].
+    + apply H ; repeat exists l' ; auto => //.
+                         move => l'' Hl'' . apply (H2 l'').
+                         apply Union_introl => //.
+    + apply H0 ; repeat exists l' ; auto => //.
+                         move => l'' Hl'' . apply (H2 l'').
+                         apply Union_intror => //.
   Qed.
 
-  Lemma scoping_reachability: forall σ l1 l2, ( σ ⊨ l1 ⇝ l2) -> (σ, {l2}) ⋖ (σ, {l1}).
+
+  Lemma scoping_reachability: forall σ l1 l2, ( σ ⊨ l1 ⇝ l2) -> (σ, {l1}) ⋖ (σ, {l2}).
   Proof.
     unfold scoping, reachability_set ; simpl.
     intros.
-    move: H1 => [l' [Hl'1 Hl'2]].
+    move: H3 => [l' [Hl'1 Hl'2]].
     induction Hl'1.
     exists l1 ; split => //.
     apply (reachability_trans σ l1 l2 l H Hl'2).
   Qed.
 
   Lemma scoping_transitivity: forall σ1 σ2 σ3 L1 L2 L3, (dom σ1) <= (dom σ2) ->
-                                                   (σ2, L2) ⋖ (σ1, L1) ->
-                                                   (σ3, L3) ⋖ (σ2, L2) ->
-                                                   (σ3, L3) ⋖ (σ1, L1).
+                                                   L2 ⪽ σ2 -> (* ADDED HYPOTHESIS *)
+                                                   (σ1, L1) ⋖ (σ2, L2) ->
+                                                   (σ2, L2) ⋖ (σ3, L3) ->
+                                                   (σ1, L1) ⋖ (σ3, L3).
   Proof.
+    intros.
     unfold scoping, reachability_set ; simpl.
     intros.
-    move: (PeanoNat.Nat.lt_le_trans l (dom σ1) _ H2 H) => H_dom.
-    move /(_ l H_dom H3):H1 => H4.
-    move /(_ l H2 H4):H0 => [l' [H5 H6]].
-    exists l' => //.
+    move : H6 => [l3 [H_in_l3 H_reach_l3]].
+    move: (PeanoNat.Nat.lt_le_trans l (dom σ1) _ H5 H) => B1.
+    apply H1; simpl => //.
+    apply H2; simpl => //.
+    exists l3 => //.
   Qed.
 
   Lemma preserving_transitivity: forall σ1 σ2 σ3 L1 L2, (σ1 ⇝ σ2 ⋖ L1) ->
                                                    (σ2 ⇝ σ3 ⋖ L2) ->
-                                                   (σ2, L2) ⋖ (σ1, L1) ->
-                                                   (dom σ1) <= (dom σ2) ->
+                                                   (σ1, L1) ⋖ (σ2, L2) ->
+                                                   (dom σ1) <= (dom σ2) ->  (* ADDED HYPOTHESIS *)
                                                    (σ1 ⇝ σ3 ⋖ L1).
   Proof.
     intros.
     unfold scoping_preservation.
-    intros σ0 L0 L Hdom A1 A2.
-    move : (H _ _ _  Hdom A1 A2) => B1.
-    move: (scoping_transitivity _ _ _ _ _ _ Hdom A1 H1) => C1.
-    apply H0 => //.
-    apply (PeanoNat.Nat.le_trans _ (dom σ1) _) => //.
+    split.
+    + apply (proj1 H).
+    + move => σ0 L0 L H_dom1 H_dom3 H_subL0 H_subL A1 A2.
+    move: (PeanoNat.Nat.le_trans _ _ _ H_dom1 H2) => H_dom2.
+    move : ((proj2 H) _ _ _  H_dom1 H_dom2 H_subL0 H_subL A1 A2) => B1.
+    move: (scoping_transitivity σ0 σ1 σ2 L0 L1 L2 H_dom1 (proj1 H) A1 H1) => C1.
+    apply (proj2 H0) => //.
+    move => l Hl.
+    apply (PeanoNat.Nat.lt_le_trans _ (dom σ1) _) => //.
+    apply (H_subL _ Hl).
   Qed.
 
 
-  Lemma preserving_regularity_degenerate: forall σ1 σ2 L, σ1 ⇝ σ2 ⋖ L -> (σ2, L) ⋖ (σ1, L).
+  Lemma preserving_regularity_degenerate: forall σ1 σ2 L, σ1 ⇝ σ2 ⋖ L -> (dom σ1) <= (dom σ2) -> (σ1, L) ⋖ (σ2, L).
   Proof.
     intros.
+    move: H => [H_subL H_pres].
+    move: (PeanoNat.Nat.le_refl (dom σ1)) => H_dom1.
     assert (L ⊆ L) as Hincluded. move => x => //.
-    assert (dom σ1 <= dom σ1) as Hdom. apply PeanoNat.Nat.le_refl.
     move: (scoping_reflexivity σ1 L L Hincluded) => Href.
-    apply (H σ1 L L Hdom) => //.
+    apply (H_pres  σ1 L L H_dom1 H0 H_subL H_subL Href Href).
     Qed.
 
   Lemma preserving_regularity: forall σ0 σ1 σ2 L L1, σ1 ⇝ σ2 ⋖ L ->
-                                                (dom σ0) <= (dom σ1) ->
-                                                (σ1, L) ⋖ (σ0, L) ->
-                                                (σ1, L1) ⋖ (σ0, L) ->
-                                                (σ2, L1) ⋖ (σ0, L).
+                                                (dom σ0) <= (dom σ2) -> (* ADDED HYPOTHESIS *)
+                                                (dom σ0) <= (dom σ1) -> (* ADDED HYPOTHESIS *)
+                                                L ⪽ σ0 -> (* ADDED HYPOTHESIS *)
+                                                L1 ⪽ σ1 -> (* ADDED HYPOTHESIS *)
+                                                (σ0, L) ⋖ (σ1, L) ->
+                                                (σ0, L) ⋖ (σ1, L1) ->
+                                                (σ0, L) ⋖ (σ2, L1).
   Proof.
     intros.
-    apply H => //.
+    apply (proj2 H) => //.
   Qed.
 
   Lemma preserving_transitivity_degenerate: forall σ1 σ2 σ3 L1 , σ1 ⇝ σ2 ⋖ L1 ->
-                                                (dom σ1) <= (dom σ2) ->
+                                                (dom σ1) <= (dom σ2) -> (* ADDED HYPOTHESIS *)
                                                  σ2 ⇝ σ3 ⋖ L1 ->
                                                  σ1 ⇝ σ3 ⋖ L1.
   Proof.
     intros.
-    move: (preserving_regularity_degenerate _ _ _ H) => A1.
-    apply: (preserving_transitivity _ _ _ _ _ H H1 A1) => //.
+    apply (preserving_transitivity σ1 σ2 σ3 L1 L1) => //.
+    apply (scoping_transitivity σ1 σ2 σ2 L1 L1 L1) => //.
+    apply (proj1 H1).
+    apply (proj2 H) => //.
+    apply (proj1 H).
+    apply (proj1 H).
   Qed.
+
+  Lemma scopability_assignment: forall σ1 σ2 σ2' L1 l l' f C ω ω',
+      σ1 ⇝ σ2 ⋖ L1 ->
+      (σ1, L1) ⋖ (σ2, {l}) ->
+      (σ1, L1) ⋖ (σ2, {l'}) ->
+      (getObj σ2 l) = Some (C, ω) ->
+      ω' = [f ↦ l']ω ->
+      σ2' = [l ↦ (C, ω')]σ2 ->
+      (σ1 ⇝ σ2' ⋖ L1) /\ ((σ1, L1) ⋖ (σ2', {l})).
+    intros.
+    split.
+    + (* σ1 ⇝ σ2' ⋖ L1 *)
+      split. apply (proj1 H).
+      move => σ0 L L0 H_dom1 H_dom2' H_subL H_subL0 A1 A2.
+      assert (dom σ2 = dom σ2') as H_dom22'. rewrite /dom H4 update_one3 => //.
+      move :(H_dom2') => H_dom2. rewrite -H_dom22' in H_dom2.
+      admit.
+      Admitted.
+
+
+
 
 
 
