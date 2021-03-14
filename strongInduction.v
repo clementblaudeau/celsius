d4c49508051f648c4aacf3661085b815bb7f540e
Blaudeau Clément <clement.blaudeau@gmail.com>
2021-03-14 15:54:09 +0100

    Cleaned the strongInduction.v file

--- strongInduction.v
+++ strongInduction.v
@@ -1,8 +1,9 @@
-Require Import ssreflect ssrbool.
-Require Import Coq.Arith.Lt.
-Require Import Coq.Arith.PeanoNat.
+(* Celsius project *)
+(* Clément Blaudeau - LAMP@EPFL 2021 *)
+(** This file defines a strong induction predicate *)
+Require Import ssreflect ssrbool Coq.Arith.Lt Coq.Arith.PeanoNat Psatz.
 
-
+(** Strong induction predicate is a strenghten version of the built-in induction *)
 Theorem strong_induction:
   forall P : nat -> Prop,
     (forall n : nat, (forall k : nat, (k < n -> P k)) -> P n) ->
@@ -10,25 +11,14 @@ Theorem strong_induction:
 Proof.
   intros.
   apply H.
-  induction n ; intros.
-  + apply PeanoNat.Nat.nlt_0_r in H0 => //.
+  induction n ; intros; try lia.
   + move : (H n) => H1.
     destruct k.
-    ++  destruct n.
-        +++ apply H1.
-            intros. apply PeanoNat.Nat.nlt_0_r in H2 => //.
-        +++ apply IHn.
-            apply PeanoNat.Nat.lt_0_succ.
-    ++ apply Lt.lt_S_n in H0.
-       apply H.
-       intros l H2.
-       apply IHn.
-       apply Lt.lt_n_Sm_le in H2.
-       apply (PeanoNat.Nat.le_lt_trans l k n) => //.
+    ++ destruct n; [apply H1 | apply IHn] ; lia.
+    ++ apply H; intros; apply IHn. lia.
 Qed.
 
-
-(* Technical lemma that appears in proofs from time to time *)
+(** Technical lemma that appears in proofs from time to time *)
 Lemma strong_induction_rewrite:
   forall (P:nat -> Prop) n, (forall k : nat, S k <= S n -> P k) <-> (forall k : nat, k < S n -> P k).
   intros.
