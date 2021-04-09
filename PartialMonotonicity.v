82a28c13a81bcd102cec6f1d4a0083b12fb20139
Blaudeau Clément <clement.blaudeau@gmail.com>
2021-04-09 19:14:16 +0200

    Cleaned PartialMonotonicity.v

--- PartialMonotonicity.v
+++ PartialMonotonicity.v
@@ -1,16 +1,14 @@
 (* Celsius project *)
 (* Clément Blaudeau - LAMP@EPFL 2021 *)
-(** This file defines the notion of reachability of a location in a given store. The set of reachable locations, starting from a given one l is transitively defined as the ones that can be accessed by following pointers in object local environments. We then define and prove basic properties around this notion. In the second part, we show the equivalence between the inductive definition and a path-based definition. This allows us to reason about paths from one location to another, especially for scopability results.  *)
+(** This file defines the notion of partial monotonicity between stores. The idea behind partial monotonicity is simple: objects that are being initialized (not warm already) can only get "warmer": the number of initialized fields can only increase. Between two stores [σ] and [σ'], it means that all objects in [σ] have more initialized fields in [σ']; it does not states anything about new objects that [σ'] can have. *)
 
 From Celsius Require Export Trees Eval Reachability Tactics Compatibility strongInduction.
-Require Import ssreflect ssrbool List.
+Require Import ssreflect ssrbool List Psatz.
 Import ListNotations.
 Open Scope nat_scope.
 
-Hint Extern 1 => unfold dom in *: dm.
-
 (** ** Definitions and notations *)
-Definition initializedFields (σ: Store) (l: Loc) (f: list Field) : Prop :=
+Definition initializedFields σ l (f: list Field) : Prop :=
   match (getObj σ l) with
   | Some (C, ω) => ((length f) <= (length ω))
   | _ => False
@@ -21,52 +19,63 @@ Definition partialMonotonicity (σ σ': Store) :=
   forall l f, (σ ⊨ l : f) -> (σ' ⊨ l : f).
 Notation "σ ⪯ σ'" := (partialMonotonicity σ σ') (at level 80).
 
-
-(* Results : *)
-
-Lemma partialMonotonicity_reflexivity : forall (σ : Store), σ ⪯ σ.
-Proof. unfold partialMonotonicity => //. Qed.
+(** ** Basic results *)
+(** The relation is trivially reflexive *)
+Lemma partialMonotonicity_reflexivity :
+  forall σ, σ ⪯ σ.
+Proof.
+  unfold partialMonotonicity => //.
+Qed.
 Hint Resolve partialMonotonicity_reflexivity: pM.
 
-Lemma initializedFields_dom : forall (σ: Store) (l: nat) (f: list Field), (σ ⊨ l : f) -> (l < (dom σ)).
-Proof.
-  unfold initializedFields, getObj.
-  induction σ; repeat light || destruct l as [|l'] || eauto.
-  apply (PeanoNat.Nat.lt_0_succ).
-  apply (Lt.lt_n_S).
-  apply IHσ in H => //.
-Qed.
-Hint Resolve initializedFields_dom: pM.
-
-Lemma initializedFields_exists : forall (σ: Store) (c: ClN) (e: Env), exists (f: list Field), ((c,e)::σ) ⊨ (dom σ) : f.
-Proof.
-  unfold initializedFields.
-  intros. exists [].
-  induction σ; intros ; simpl ; eauto using le_0_n.
-  - destruct σ, a => //. apply le_0_n.
-Qed.
-
-Lemma partialMonotonicity_domains : forall (σ σ': Store), σ ⪯ σ' -> (dom σ) <= (dom σ').
-Proof.
-  intros.
-  unfold partialMonotonicity in H.
-  move : (initializedFields_dom σ') => Hσ'.
-  destruct (σ) eqn:Σ; eauto using le_0_n.
-  - destruct o.
-    case : (initializedFields_exists s c e) => f Hf.
-    eauto using Lt.lt_le_S.
-Qed.
-Hint Resolve partialMonotonicity_domains: pM.
-
-
+(** The relation is trivially transitive *)
 Lemma partialMonotonicity_transitivity : forall (σ1 σ2 σ3 : Store), (σ1 ⪯ σ2) -> (σ2 ⪯ σ3) -> (σ1 ⪯ σ3).
 Proof.
   unfold partialMonotonicity; auto.
 Qed.
 Hint Resolve partialMonotonicity_transitivity: pM.
 
+(** Initialized fields are within the domain *)
+Lemma initializedFields_dom :
+  forall σ l f,
+    (σ ⊨ l : f) -> (l < (dom σ)).
+Proof.
+  unfold initializedFields, getObj.
+  induction σ; destruct l as [| l']; steps; try lia.
+  eapply Le.le_n_S, IHσ; rewrite_any; eauto.
+Qed.
+Hint Resolve initializedFields_dom: pM.
 
-Lemma partialMonotonicity_assignment : forall (σ σ': Store) (l: Loc) (C: ClN) (ω ω': Env),
+(** Technical result: an object always have 0 or more fields initialized *)
+Lemma initializedFields_exists :
+  forall σ c e,
+  exists f, ((c,e)::σ) ⊨ (dom σ) : f.
+Proof.
+  unfold initializedFields.
+  intros. exists [].
+  induction σ; intros ; simpl ; try lia.
+  - destruct σ, a; steps; lia.
+Qed.
+Hint Resolve initializedFields_exists: pM.
+
+(** We have a result on the store sizes *)
+Lemma partialMonotonicity_domains:
+  forall σ σ',
+    σ ⪯ σ' -> dom σ <= dom σ'.
+Proof.
+  unfold partialMonotonicity; intros.
+  destruct σ eqn:Σ; simpl; [lia |].
+  destruct o.
+  case : (initializedFields_exists s c e) => f Hf.
+  eauto using Lt.lt_le_S with pM.
+Qed.
+Hint Resolve partialMonotonicity_domains: pM.
+
+
+(** ** Main Monotonicity result *)
+(** We start with two technical results on partial monotonicity for assignment (update) and fresh location *)
+Lemma partialMonotonicity_assignment :
+  forall σ σ' l C ω ω',
     getObj σ l = Some (C, ω) ->
     length ω <= length ω' ->
     σ' = [l ↦ (C, ω')]σ ->
@@ -74,51 +83,47 @@ Lemma partialMonotonicity_assignment : forall (σ σ': Store) (l: Loc) (C: ClN) 
 Proof.
   unfold partialMonotonicity, initializedFields.
   intros.
-  move: (PeanoNat.Nat.eq_dec l l0) => [H4 | H4].
-  - rewrite H1 H4 getObj_update1 => //.
-    apply (initializedFields_dom _ _ f H2).
-    unfold initializedFields in H2.
-    rewrite -H4 H in H2 => //.
-    apply: (PeanoNat.Nat.le_trans _ _ _ H2 H0).
-  - move: (getObj_update2 σ (C, ω') l l0 ((getObj_dom σ (C,ω) l) H) H4)=>H5.
-    rewrite H1 H5 => //.
+  destruct_eq (l = l0); subst;
+    repeat rewrite_any;
+    rewrite getObj_update1 || rewrite getObj_update2;
+    eauto using getObj_dom.
 Qed.
 Hint Resolve partialMonotonicity_assignment: pM.
-
 
 Lemma partialMonotonicity_freshness : forall (σ: Store) (c: ClN) (ρ: Env),
     σ ⪯ σ ++ [(c, ρ)].
 Proof.
-  unfold partialMonotonicity.
-  unfold initializedFields.
+  unfold partialMonotonicity, initializedFields.
   induction σ ; destruct l => //.
   apply IHσ => //.
 Qed.
 Hint Resolve partialMonotonicity_freshness: pM.
 
-Lemma partialMonotonicity_InitMaintained: forall n,  InitMaintained partialMonotonicity n.
+(** Then we have the main result *)
+Theorem partialMonotonicity_theorem:
+  forall n, forall e σ σ' ρ v v',
+      ⟦e⟧(σ, ρ, v)(n) = Success v' σ' ->
+      σ ⪯ σ'.
 Proof.
-  intros. apply FreshnessInitMaintained; unfold Reflexive, Transitive, Assignment, Freshness; eauto with pM.
+  apply (EvalMaintainedProp partialMonotonicity);
+    try (intros; apply FreshnessInitMaintained);
+    unfoldProps;
+    eauto with pM.
 Qed.
-Hint Resolve partialMonotonicity_InitMaintained: pM.
+Hint Resolve partialMonotonicity_theorem: pM.
 
-Theorem partialMonotonicity_theorem: forall (n : nat), forall (e: Expr) (σ σ': Store) (ρ: Env) (v v': Value),
-      ⟦e⟧(σ, ρ, v)(n) = (Success v' σ') -> σ ⪯ σ'.
+(** ** Other conservation results *)
+(** As a consequence, we have monotonicity on the sizes of stores *)
+Theorem partialMonotonicity_theorem_dom:
+  forall n, forall e σ σ' ρ v v',
+      ⟦e⟧(σ, ρ, v)(n) = Success v' σ' ->
+      dom σ <= dom σ'.
 Proof.
-  apply (EvalMaintainedProp partialMonotonicity); unfold Reflexive, Transitive, Assignment, Freshness; eauto with pM.
-
+  eauto with pM.
 Qed.
-
-
-Theorem partialMonotonicity_theorem_dom: forall (n : nat), forall (e: Expr) (σ σ': Store) (ρ: Env) (v v': Value),
-      ⟦e⟧(σ, ρ, v)(n) = (Success v' σ') -> (dom σ) <= (dom σ').
-Proof.
-  intros. apply partialMonotonicity_theorem in H.
-    by apply partialMonotonicity_domains.
-Qed.
-
 Hint Resolve partialMonotonicity_theorem_dom: pM.
 
+(** Similarly, we have a result on evaluation of lists *)
 Theorem partialMonotonicity_theorem_list_dom: forall n el σ σ' ρ ψ ρ',
     ⟦_ el _⟧(σ, ρ, ψ)(n) = (Success_l ρ' σ') -> (dom σ) <= (dom σ').
 Proof.
@@ -128,18 +133,12 @@ Proof.
 Qed.
 Hint Resolve partialMonotonicity_theorem_list_dom: pM.
 
-
-
+(** If we combine partial monotonicity with compatibility (objects do no change type), we can prove that warm objects stay warm *)
 Lemma partialMonotonicity_warm_monotone: forall σ σ' l, σ ⪯ σ' -> σ ⊆ σ' -> (σ ⊨ l : warm) -> (σ' ⊨ l : warm).
 Proof.
-  unfold reachable_warm.
-  intros σ σ' l H_pm H_c [C [ω [args [fields[ methods [H2 [H3 H4]] ]]]]].
-  unfold partialMonotonicity, initializedFields in H_pm.
-  move /(_ l fields):H_pm => H_pm.
-  rewrite H2 in H_pm.
-  move /(_ H4):H_pm => H_pm.
-  unfold compatible in H_c.
-  move /(_ l C ω H2):H_c => [ω' H_c].
-  rewrite H_c in H_pm.
-  exists C, ω', args, fields, methods => //.
+  unfold partialMonotonicity, initializedFields, reachable_warm; steps.
+  specialize (H l fields).
+  repeat rewrite_any.
+  eapply H0 in H2; steps.
+  repeat eexists; eauto.
 Qed.
